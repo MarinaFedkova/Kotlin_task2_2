d4f3358ef472c5729c0b33fd15d715c231a9695c
Marina Fedkova <mzaloznaya@ya.ru>
2021-01-16 12:54:33 +0300

    rework 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
-    val likes = 11
-    if (likes == 1 || likes > 11 && likes % 10 == 1)
+    val likes = 2
+    if (likes % 100 == 11)
+        println("Понравилось $likes людям")
+    else if (likes == 1 || (likes > 11 && likes % 10 == 1))
         println("Понравилось $likes человеку")
     else
         println("Понравилось $likes людям")
